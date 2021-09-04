b4287c2e22cc754095e6e61ec0b8842be4ffc3af
C. Lewis <ctj.lewis@icloud.com>
2021-09-04 08:18:28 -0500

    fix: tests

--- test/e2e/exports.test.ts
+++ test/e2e/exports.test.ts
@@ -1,4 +1,3 @@
-import { join, resolve } from 'path';
 import { readFileSync } from 'fs';
 import { execWithCache } from '../utils/shell';
 
@@ -10,7 +9,6 @@ shell.config.silent = false;
 const testDir = 'e2e';
 const fixtureName = 'build-exports';
 const stageName = `stage-${fixtureName}`;
-const stageDir = resolve(process.cwd(), stageName);
 
 describe('tsdx build :: exports', () => {
   beforeAll(() => {
@@ -18,41 +16,36 @@ describe('tsdx build :: exports', () => {
     util.setupStageWithFixture(testDir, stageName, fixtureName);
   });
 
-  it('should compile files into a dist directory', () => {
-    const output = execWithCache('node ../dist/index.mjs build');
-
-    expect(shell.test('-f', 'dist/index.mjs')).toBeTruthy();
-    expect(shell.test('-f', 'dist/index.d.ts')).toBeTruthy();
-    expect(output.code).toBe(0);
-  });
-
   describe('library exports', () => {
-    const json = readFileSync(join(stageDir, 'package.json'), 'utf-8');
-    const packageJson = JSON.parse(json);
-
     it('should set package.json `exports` field', () => {
+      const json = readFileSync(`package.json`, 'utf-8');
+      const packageJson = JSON.parse(json);
       expect(packageJson.exports).toBeTruthy();
     });
 
     it('should export `my-package/path/to/module` as my-package/path/to/module.mjs', () => {
+      const json = readFileSync(`package.json`, 'utf-8');
+      const packageJson = JSON.parse(json);
       expect(packageJson.exports['.']).toEqual('./dist/index.mjs');
       expect(packageJson.exports['./*']).toEqual('./dist/*.mjs');
     });
 
+    it('should build the project', () => {
+      execWithCache('node ../dist/index.mjs build');
+    });
+
     it('should export named members properly', () => {
-      const mjs = join(stageDir, 'exports.named.mjs');
-      const output = execWithCache(`node ${mjs}`);
-      expect(output.code).toBe(0);
+      const named = execWithCache(`node exports.named.mjs`);
+      expect(named.code).toBe(0);
     });
 
     it('should export default members properly', () => {
-      const mjs = join(stageDir, 'exports.default.mjs');
-      const output = execWithCache(`node ${mjs}`);
-      expect(output.code).toBe(0);
+      const def = execWithCache(`node exports.default.mjs`);
+      expect(def.code).toBe(0);
     });
   });
 
-  afterAll(() => {
-    util.teardownStage(stageName);
-  });
+  // afterAll(() => {
+  //   util.teardownStage(stageName);
+  // });
 });
