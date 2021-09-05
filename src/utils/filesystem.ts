32cdceeef9717c8c60978fbc378fdacd8c541738
C. Lewis <ctj.lewis@icloud.com>
2021-09-04 20:18:11 -0500

    chore: use git for default author, not NPM

--- src/utils/filesystem.ts
+++ src/utils/filesystem.ts
@@ -90,32 +90,15 @@ export const getAppPackageJson = async () => {
 };
 
 export const getAuthorName = () => {
-  let author = '';
-
-  author = shell
-    .exec('npm config get init-author-name', { silent: true })
-    .stdout.trim();
-  if (author) return author;
-
-  author = shell
+  const author = shell
     .exec('git config --global user.name', { silent: true })
     .stdout.trim();
-  if (author) {
-    setAuthorName(author);
-    return author;
-  }
 
-  author = shell
-    .exec('npm config get init-author-email', { silent: true })
-    .stdout.trim();
-  if (author) return author;
-
-  author = shell
+  const email = shell
     .exec('git config --global user.email', { silent: true })
     .stdout.trim();
-  if (author) return author;
 
-  return author;
+  return `${author} <${email}>`;
 };
 
 export const setAuthorName = (author: string) => {
